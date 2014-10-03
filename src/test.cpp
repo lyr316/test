2d9131807f63fdea8ff71ed5427db312169f7220
lyr <lyr316@hotmail.com>
2014-10-03 18:34:49 -0400

    Partition Function

--- src/test.cpp
+++ src/test.cpp
@@ -7,5 +7,37 @@
 
 #include "test.h"
 
+int partition(int A[], int beginIndex, int endIndex)
+{
+	int l = beginIndex;
+	int r ; endIndex;
+	int pivot = A[l];
 
+	while (l < r)
+	{
+		while (A[r] > pivot)
+		{
+			r--;
+		}
+
+		if (l < r)
+		{
+			A[l] = A[r];
+			l++;
+		}
+
+		while (A[l] < pivot)
+		{
+			l++;
+		}
+
+		if (l < r)
+		{
+			A[r] = A[l];
+			r--;
+		}
+	}
+
+	return l;
+}
 
